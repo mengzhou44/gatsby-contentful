ed004f7decd8a806e58cb5d77ea61acc5dd7ef11
Meng Zhou <v-menzhou@expediagroup.com>
2022-06-18 21:37:54 -0500

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,24 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,63 @@
 import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <ContactsContainer>
+      {data.contacts.edges.map(edge => (
+        <Contact key={edge.node.id}>
+          <Image
+            image={edge.node.photo.gatsbyImageData}
+            alt="alt"
+            placeholder="blurred"
+          />
+          <ContactInfo>
+            {edge.node.lastName}, {edge.node.firstName}, Phone:{" "}
+            {edge.node.phone}
+          </ContactInfo>
+        </Contact>
+      ))}
+    </ContactsContainer>
+  )
 }
+
+export const query = graphql`
+  query {
+    contacts: allContentfulContacts {
+      edges {
+        node {
+          lastName
+          firstName
+          id
+          phone
+          photo {
+            gatsbyImageData(layout: CONSTRAINED)
+          }
+        }
+      }
+    }
+  }
+`
+
+const ContactsContainer = styled.div`
+  margin: 0 auto;
+  width: 400px;
+`
+
+const Contact = styled.div`
+  padding: 2rem;
+  font-size: 1.6rem;
+  color: #fcfcfc;
+  margin-bottom: 2rem;
+  background-color: #bd9015;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-radius: 5px;
+`
+
+const ContactInfo = styled.div``
+
+const Image = styled(GatsbyImage)``
